a2d5884d29d1e99833684e42f3fa77ca51a9a243
wedleysilva <wedleysilva210@gmail.com>
2023-06-06 14:49:12 -0300

    conectei categoria à livros com o ManyToMany

--- livraria/models.py
+++ livraria/models.py
@@ -29,9 +29,7 @@ class Livro(models.Model):
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
-    categoria = models.ForeignKey(
-            Categoria, on_delete=models.PROTECT, related_name="livros"
-    )
+    categoria = models.ManyToManyField(Categoria, related_name="livros")
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
     autores = models.ManyToManyField(Autor, related_name="livros")
 
