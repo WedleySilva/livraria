f645712ae7ccc8df7472033dad58480b68943aea
wedleysilva <wedleysilva2105@gmail.com>
2023-06-12 16:33:04 -0300

    "PARA ACESSAR O SISTEMA PRECISA ESTAR LOGADO"

--- config/settings.py
+++ config/settings.py
@@ -86,6 +86,13 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissions",
+    ],
+
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
