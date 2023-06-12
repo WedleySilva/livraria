c4472cff9319cee514fca0526e3ae6c94e082f86
wedleysilva <wedleysilva2105@gmail.com>
2023-06-12 16:36:12 -0300

    permissoẽs com DRF aplicadas no settings e views

--- livraria/views.py
+++ livraria/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from livraria.models import Autor, Categoria, Editora, Livro
 from livraria.serializers import AutorSerializer, CategoriaSerializer, EditoraSerializer, LivroSerializer
@@ -7,19 +8,22 @@ from livraria.serializers import AutorSerializer, CategoriaSerializer, EditoraSe
 class AutorViewSet(ModelViewSet):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
+    permission_classes = [IsAuthenticated]
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
-
+    permission_classes = [IsAuthenticated]
 
