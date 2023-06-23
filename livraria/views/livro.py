57cc7934f73db7136cc2e815829ee91fe3d39f3b
wedleysilva <wedleysilva2105@gmail.com>
2023-06-23 11:21:02 -0300

    serializers e views alterados

---
+++ livraria/views/livro.py
@@ -0,0 +1,10 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+
+from livraria.models import Livro
+from livraria.serializers.livro import LivroSerializer
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    permission_classes = [IsAuthenticated]
+
